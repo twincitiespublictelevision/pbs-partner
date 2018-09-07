affd51241fbcb3abb1ab89dada1ff2550a3e4f2b
Augustus Mayo <gusmayo@gmail.com>
2018-09-07 07:50:26 -0500

    Add plugin exports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import PBSPlayer from './PBSPlayer';
+import ResumePlugin from './plugins/Resume';
+import GoogleAnalyticsPlugin from './plugins/GoogleAnalytics';
+
+export const GoogleAnalytics = GoogleAnalyticsPlugin;
+export const Resume = ResumePlugin;
 
 export default PBSPlayer;
