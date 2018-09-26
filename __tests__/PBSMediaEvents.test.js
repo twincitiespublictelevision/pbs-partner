21d6bfce4b4f5e86f1928c023d1983aa80933fc6
Augustus Mayo <gusmayo@gmail.com>
2018-09-26 07:50:54 -0500

    More testing

--- __tests__/PBSMediaEvents.test.js
+++ __tests__/PBSMediaEvents.test.js
@@ -1,7 +1,7 @@
 import PBSPlayer from '../src/PBSPlayer';
 import { origin, mockPlayerFactory, mockWindowFactory, mockMessageEventFactoryFactory } from "./setup";
 
-describe('PBSMediaEvents', function() {
+describe('MediaStart', function() {
   let env,
     player,
     api,
@@ -21,7 +21,82 @@ describe('PBSMediaEvents', function() {
     makeEvent = mockMessageEventFactoryFactory(player.contentWindow);
   });
 
-  it('should work', function() {
+  it('should send MediaStart exactly once', function() {
+    return expect(true).toEqual(true);
+  });
+
+  it('should send MediaStart again after complete', function() {
+    return expect(true).toEqual(true);
+  });
+
+  it('should send MediaStart again after destroy', function() {
+    return expect(true).toEqual(true);
+  });
+});
+
+describe('MediaStop', function() {
+  let env,
+    player,
+    api,
+    messageMock,
+    makeEvent;
+
+  beforeEach(function() {
+    messageMock = jest.fn();
+    player = mockPlayerFactory();
+    player.contentWindow.postMessage = messageMock;
+
+    env = mockWindowFactory();
+
+    api = new PBSPlayer({player: player, env: env});
+    api.setPlayer(player);
+
+    makeEvent = mockMessageEventFactoryFactory(player.contentWindow);
+  });
+
+  it('should send not send MediaStop before MediaStart', function() {
+    return expect(true).toEqual(true);
+  });
+
+  it('should send MediaStop on complete', function() {
+    return expect(true).toEqual(true);
+  });
+
+  it('should send MediaStop on destroy', function() {
+    return expect(true).toEqual(true);
+  });
+
+  it('should include duration and reach', function() {
+    return expect(true).toEqual(true);
+
+  });
+});
+
+describe('Progress tracking', function() {
+  let env,
+    player,
+    api,
+    messageMock,
+    makeEvent;
+
+  beforeEach(function() {
+    messageMock = jest.fn();
+    player = mockPlayerFactory();
+    player.contentWindow.postMessage = messageMock;
+
+    env = mockWindowFactory();
+
+    api = new PBSPlayer({player: player, env: env});
+    api.setPlayer(player);
+
+    makeEvent = mockMessageEventFactoryFactory(player.contentWindow);
+  });
+
+  it('should include duration and reach on MediaStop', function() {
+    return expect(true).toEqual(true);
+  });
+
+  it('should reset duration and reach on MediaStop', function() {
     return expect(true).toEqual(true);
   });
 });
--- __tests__/PBSPlayer.test.js
+++ __tests__/PBSPlayer.test.js
@@ -1,8 +1,7 @@
 import PBSPlayer from '../src/PBSPlayer';
 import { origin, mockPlayerFactory, mockWindowFactory, mockMessageEventFactoryFactory } from "./setup";
 
-describe('PBSPlay' +
-  'er', function() {
+describe('Initialize', function() {
   let env,
     player,
     api,
@@ -22,7 +21,53 @@ describe('PBSPlay' +
     makeEvent = mockMessageEventFactoryFactory(player.contentWindow);
   });
 
-  it('should work', function() {
-    return expect(true).toEqual(true);
+  it('should trigger connected on first communication', function() {
+    let handlerMock = jest.fn();
+    api.on('connected', handlerMock);
+    api.trigger('message');
+
+    return expect(handlerMock.mock.calls.length).toBe(1);
+  });
+});
+
+describe('Completion', function() {
+  let env,
+    player,
+    api,
+    messageMock,
+    makeEvent;
+
+  beforeEach(function() {
+    messageMock = jest.fn();
+    player = mockPlayerFactory();
+    player.contentWindow.postMessage = messageMock;
+
+    env = mockWindowFactory();
+
+    api = new PBSPlayer({player: player, env: env});
+    api.setPlayer(player);
+
+    makeEvent = mockMessageEventFactoryFactory(player.contentWindow);
+  });
+
+  it('should trigger complete when pause occurs at end of video', function() {
+    let handlerMock = jest.fn();
+
+    let result = new Promise((res) => {
+      api.on('complete', () => {
+        handlerMock();
+        res();
+      });
+    });
+
+    api.trigger('message');
+    env.dispatchEvent(makeEvent('video::playing'));
+    env.dispatchEvent(makeEvent('getDuration::5'));
+    env.dispatchEvent(makeEvent('video::paused'));
+    env.dispatchEvent(makeEvent('getPosition::5'));
+
+    return result.then(() => {
+      return expect(handlerMock.mock.calls.length).toBe(1);
+    });
   });
 });
